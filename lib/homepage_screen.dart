5729edf758c1ac859847078e4a1b8d07d69d02e2
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-10-20 20:45:31 +0700

    bg1 + rombak row and column

--- lib/homepage_screen.dart
+++ lib/homepage_screen.dart
@@ -10,79 +10,92 @@ class Homepage_Screen extends StatelessWidget{
     return Scaffold(
         body: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Container(
-                    padding: EdgeInsets.only(top: 100,left: 50),
-                    child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Row(
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Text(
-                        "Go Green",
-                        style: informationTextStyle,
+                      Container(
+                          padding: EdgeInsets.only(top:100, left: 57),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Go Green",
+                                style: informationTextStyle,
+                              ),
+                              Text(
+                                  "Selamatkan bumi kita"
+                              ),
+                            ],
+                          )
                       ),
-                      Text("Selamatkan bumi kita")
+                      Image.asset(
+                        "assets/png/bg1.png",
+                        width: 200,
+                        height: 300,
+                        alignment: Alignment.topRight,
+                      )
                     ],
-                  )
+                  ),
                 ),
                 Container(
-                  padding: EdgeInsets.only(top: 100, left: 50, right: 50, bottom: 10),
+                  padding: EdgeInsets.only(left: 50, right: 50, bottom: 10),
                   child: Column(
                     children: <Widget>[
                       Image.asset(
                         "assets/png/artboard.png",
                         width: 270,
-                        height: 290,
+                        height: 270,
                       ),
                       Padding(
                           padding: EdgeInsets.only(top: 10)
                       ),
                       Text(
-                          "Aplikasi bank sampah yang dapat meringankan pekerjaan anda",
-                          textAlign: TextAlign.center,
+                        "Aplikasi bank sampah yang dapat meringankan pekerjaan anda",
+                        textAlign: TextAlign.center,
                       ),
                     ],
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.only(top:15, left: 80, right: 80),
-                  height: 60,
-                  child: TextButton(
-                    style:
-                    TextButton.styleFrom(backgroundColor: Colors.green),
-                    onPressed: () {},
-                    child: Text(
-                      "Buat Akun Baru",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15
-                      ),
-                    ),
-                ),
-                ),
-                Container(
-                    padding: EdgeInsets.only(top:15, left: 80, right: 80),
-                    height: 60,
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: Colors.green, width: 2)
-                      ),
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                          return Login_Screen();
-                        }));
-                      },
-                      child: Text(
-                          "Login",
+                  margin: EdgeInsets.only(left: 80, right: 80),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      TextButton(
+                        style:
+                        TextButton.styleFrom(backgroundColor: Colors.green),
+                        onPressed: () {},
+                        child: Text(
+                          "Buat Akun Baru",
+                          textAlign: TextAlign.center,
                           style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 15
-                          )
+                              color: Colors.white,
+                              fontSize: 15
+                          ),
+                        ),
                       ),
-
-                    ),
-                )
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Colors.green, width: 2)
+                        ),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return Login_Screen();
+                          }));
+                        },
+                        child: Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 15
+                            )
+                        ),
+                      ),
+                    ],
+                  )
+                ),
               ],
             )
         )
