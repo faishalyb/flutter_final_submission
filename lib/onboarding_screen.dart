e5ae48fe874194f1ea1f7b5854064ac7874823e0
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-10-21 19:52:17 +0700

    onboarding 3 page

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,26 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'login_screen.dart';
+var informationTextStyle = const TextStyle(fontFamily: 'Gabarito');
+
 
 class Onboarding_Screen extends StatelessWidget {
   const Onboarding_Screen({Key? key}) : super(key: key);
-  final String assetName = ('assets/svg/onboarding1.svg');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.tealAccent,
       body: SafeArea(
-        child: Stack(
+        child: PageView(
           children: <Widget>[
-            Positioned.fill(
-                child: SvgPicture.asset(
-                  assetName,
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  fit: BoxFit.cover,
-                )
-            )
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                      child: SvgPicture.asset(
+                        "assets/svg/onboarding1.svg",
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        fit: BoxFit.cover,
+                      )
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                      child: SvgPicture.asset(
+                        "assets/svg/onboarding2.svg",
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        fit: BoxFit.cover,
+                      )
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                      child: SvgPicture.asset(
+                        "assets/svg/onboarding3.svg",
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        fit: BoxFit.cover,
+                      )
+                  )
+                ],
+              ),
+            ),
+            Container(
+
+            ),
           ],
         ),
       )
