f3b25f046811b0b4f453ffccddedc4d7d0d3f345
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-10-22 21:42:22 +0700

    onboarding finished

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'faishalyb_final_submission',
       theme: ThemeData(),
-      home: const Onboarding_Screen(),
+      home: Onboarding_Screen(),
     );
   }
 }
--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -4,144 +4,177 @@ import 'login_screen.dart';
 var informationTextStyle = const TextStyle(fontFamily: 'Gabarito');
 
 
-class Onboarding_Screen extends StatelessWidget {
-  const Onboarding_Screen({Key? key}) : super(key: key);
+class Onboarding_Screen extends StatefulWidget {
+  @override
+  State<Onboarding_Screen> createState() => _Onboarding_ScreenState();
+}
+
+class _Onboarding_ScreenState extends State<Onboarding_Screen> {
+  final PageController _pageController = PageController();
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: PageView(
+        child: Stack(
           children: <Widget>[
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Positioned.fill(
-                      child: SvgPicture.asset(
-                        "assets/svg/onboarding1.svg",
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        fit: BoxFit.cover,
-                      )
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 700,left: 70, right: 70),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        TextButton(
-                          style: TextButton.styleFrom(backgroundColor: Colors.orange),
-                          onPressed: () {},
-                          child: Text(
-                            "Berikutnya",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: "Gabarito"
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+            PageView(
+              controller: _pageController,
+              onPageChanged: (int page){
+                setState(() {
+                  _currentPage = page;
+                });
+              },
+              children: <Widget>[
+                Container(
+                  child: SvgPicture.asset(
+                    "assets/svg/onboarding1.svg",
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    fit: BoxFit.cover,
                   )
-                ],
-              ),
-            ),
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Positioned.fill(
-                      child: SvgPicture.asset(
-                        "assets/svg/onboarding2.svg",
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        fit: BoxFit.cover,
-                      )
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 700,left: 70, right: 70),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                ),
+                Container(
+                    child: SvgPicture.asset(
+                      "assets/svg/onboarding2.svg",
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      fit: BoxFit.cover,
+                    )
+                ),
+                Container(
+                    child: Stack(
                       children: <Widget>[
-                        TextButton(
-                          style: TextButton.styleFrom(backgroundColor: Colors.orange),
-                          onPressed: () {},
-                          child: Text(
-                            "Berikutnya",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: "Gabarito"
-                            ),
-                          ),
+                        SvgPicture.asset(
+                          "assets/svg/onboarding3.svg",
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height,
+                          fit: BoxFit.cover,
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Positioned.fill(
-                      child: SvgPicture.asset(
-                        "assets/svg/onboarding3.svg",
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        fit: BoxFit.cover,
-                      )
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 600,left: 80, right: 80),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        TextButton(
-                          style: TextButton.styleFrom(backgroundColor: Colors.teal),
-                          onPressed: () {},
-                          child: Text(
-                            "Buat Akun Baru",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15
-                            ),
-                          ),
-                        ),
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: Colors.teal, width: 2)
-                          ),
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context){
-                                return Login_Screen();
-                              }));
-                            },
-                            child: Text(
-                              "Login",
+                        Container(
+                          margin: EdgeInsets.only(top: 560,left: 80, right: 80),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Text(
+                                  "Jadilah bagian dari aksi lindungi Bumi dengan memanfaatkan dan mengelola sampah dengan benar!",
+                              textAlign: TextAlign.justify,
                               style: TextStyle(
-                                color: Colors.teal,
+                                fontFamily: "Gabarito",
+                                color: Colors.grey,
                                 fontSize: 15
                               ),
-                            )
+                              )
+                            ],
+                          ),
                         )
                       ],
-                    ),
-                  )
-                ],
-              ),
+                    )
+                ),
+                Container(
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                          child: SvgPicture.asset(
+                            "assets/svg/onboarding4.svg",
+                            alignment: Alignment.center,
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height,
+                            fit: BoxFit.cover,
+                          )
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 600,left: 80, right: 80),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            TextButton(
+                              style: TextButton.styleFrom(backgroundColor: Colors.teal),
+                              onPressed: () {},
+                              child: Text(
+                                "Buat Akun Baru",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15
+                                ),
+                              ),
+                            ),
+                            OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                    side: BorderSide(color: Colors.teal, width: 2)
+                                ),
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context){
+                                    return Login_Screen();
+                                  }));
+                                },
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.teal,
+                                      fontSize: 15
+                                  ),
+                                )
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            
+            Visibility(
+                visible: _currentPage < 3,
+                child: Container(
+                  padding: EdgeInsets.only(top: 700,left: 70, right: 70),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      TextButton(
+                        style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                        onPressed: () {
+                          if (_currentPage < 3) {
+                            // If not on the last page, navigate to the next page
+                            _pageController.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.ease,
+                            );
+                          } else {
+                            // If on the last page, navigate to a different screen (for example, Login_Screen)
+                            Navigator.push(context, MaterialPageRoute(builder: (context){
+                              return Login_Screen();
+                            }));
+                          }
+                        },
+                        child: Text(
+                          _currentPage < 2 ? "Berikutnya" : "Selesai",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: "Gabarito"
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
