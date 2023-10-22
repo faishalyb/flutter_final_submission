77f2015b62463632abce70999a8b6b271ea0a50e
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-10-22 21:46:58 +0700

    edit some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:final_submission/onboarding_screen.dart';
 import 'package:flutter/material.dart';
-import 'homepage_screen.dart';
+import 'gadipake.dart';
 
 void main() {
   runApp(const MyApp());
